42e940f8d871fea5fa900cf5907339d330a3d024
Sheriff Oyewusi <sheriff_oyewusi@hotmail.com>
2025-04-01 19:29:12 -0700

    This method is commonly used for DOM manipulation

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 let titleElement = document.getElementById("title");
 console.log(titleElement);
+
+const header = document.querySelector("#header");
+const paragraph = document.querySelector(".text");
+const container = document.querySelector(".container");
+
+console.log(header);
+console.log(paragraph);
+console.log(container);
